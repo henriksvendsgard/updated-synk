b75626e0ea8ad90fa417873425c10afe071746f7
Henrik Svendsgård <hes@itumx.no>
2025-08-26 12:29:24 +0200

    style(card): updated card styling to work with outline

--- packages/jokul/src/components/card/development/Example.tsx
+++ packages/jokul/src/components/card/development/Example.tsx
@@ -4,6 +4,10 @@ import {
     fakturainfoExampleProps,
 } from "./examples/FakturainfoExample.js";
 import {
+    NavCardExample,
+    navCardExampleProps,
+} from "./examples/NavCardExample.js";
+import {
     StatuskortExample,
     statuskortExampleProps,
 } from "./examples/StatuskortExample.js";
@@ -20,6 +24,11 @@ export default function Example() {
                 title="Informasjon om forfalt faktura"
                 knobs={fakturainfoExampleProps}
                 component={FakturainfoExample}
+            />
+            <DevExample
+                title="Navigasjonskort"
+                knobs={navCardExampleProps}
+                component={NavCardExample}
             />
         </>
     );
